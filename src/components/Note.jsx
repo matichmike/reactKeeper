a26b3b43cd9eff8cb457d3882f28427260112549
Michael Matich <matich.mike@gmail.com>
2021-02-12 19:56:40 -0500

    adding notes implemented

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Note({title, content}) {
+function Note({title, content, onDelete, id}) {
+
+  function handleClick() {
+    onDelete(id)
+  }
+
   return (
     <div className="note">
       <h1>{title}</h1>
       <p>{content}</p>
+      <button onClick={handleClick}>Delete</button>
     </div>
   )
 }
